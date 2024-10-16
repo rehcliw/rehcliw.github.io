1f22f66386a898ff41f3d2a8bfd7443d922f463b
rehcliw <rehcliw@gmail.com>
2024-10-16 19:59:36 -0400

    linting

--- scripts/hydra-1.js
+++ scripts/hydra-1.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas-1");
+const canvas = document.getElementById('canvas-1');
 
 // fix resolution
 canvas.width = 1024;
@@ -7,7 +7,7 @@ canvas.height = 1024;
 var hydra = new Hydra({
   detectAudio: false,
   enableStreamCapture: false,
-})
+});
 
 var r = 1;
 var g = 0;
@@ -17,13 +17,14 @@ var mod = 1.9;
 osc(7, 0, 0.5)
   .rotate(0, 0.3)
   .kaleid(4)
-  .color(()=>r,()=>g,()=>b)
-  .modulateRotate(
-    shape(4,0.9,2.9)
-      .mult(o0)
-      .mask(noise(1,0),0)
-      .repeat(2,2)
-    ,()=>mod/1
+  .color(
+    () => r,
+    () => g,
+    () => b
   )
-  .scale(1,()=>window.innerHeight/window.innerWidth)
-  .out(o0)
+  .modulateRotate(
+    shape(4, 0.9, 2.9).mult(o0).mask(noise(1, 0), 0).repeat(2, 2),
+    () => mod / 1
+  )
+  .scale(1, () => window.innerHeight / window.innerWidth)
+  .out(o0);
